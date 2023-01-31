fafc292f72e9a2783e761ab75ff7d0f77a027536
vlada <buzinga3496@gmail.com>
2023-01-31 19:37:47 +0300

    added annotation

--- backend/src/main/java/com/example/backend/manual/controllers/GetUserManualsForUse.java
+++ backend/src/main/java/com/example/backend/manual/controllers/GetUserManualsForUse.java
@@ -7,6 +7,7 @@ import com.example.backend.manual.pojo.ManualJSON;
 import com.example.backend.manual.repository.ManualRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,7 @@ public class GetUserManualsForUse {
     ManualRepository manualRepository;
 
     @GetMapping("/getMyManualsForUse")
+    @Transactional
     public List<ManualJSON> getMuManualsForUse(Authentication authentication){
         User user = ((UserDetailsImpl) authentication.getPrincipal()).getUser();
         var manualOpt = manualRepository.findAllByUser(user);
