1d7dc35a234811b48c0afad7957e7b9238369dcf
vlada <buzinga3496@gmail.com>
2023-01-31 19:17:22 +0300

    refactor

--- backend/src/main/java/com/example/backend/manual/service/ManualServiceImpl.java
+++ backend/src/main/java/com/example/backend/manual/service/ManualServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.backend.manual.model.Manual;
 import com.example.backend.manual.repository.ManualRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.Optional;
 
 @Service
@@ -11,12 +12,12 @@ public class ManualServiceImpl implements ManualService {
     private final ManualRepository manualRepository;
 
     @Autowired
-    public ManualServiceImpl(ManualRepository manualRepository){
+    public ManualServiceImpl(ManualRepository manualRepository) {
         this.manualRepository = manualRepository;
     }
 
     @Override
-    public Optional<Manual> save(Manual manual){
+    public Optional<Manual> save(Manual manual) {
         return Optional.of(manualRepository.save(manual));
     }
 
