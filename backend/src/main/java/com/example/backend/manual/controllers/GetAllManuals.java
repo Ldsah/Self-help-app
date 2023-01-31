00dca12830990315b047df741b1550ba5e4f4343
vlada <buzinga3496@gmail.com>
2023-01-31 14:21:21 +0300

    добавлена возможность для авторизованных пользователей получать список всех методичек, доступных для использования

--- backend/src/main/java/com/example/backend/manual/controllers/GetAllManuals.java
+++ backend/src/main/java/com/example/backend/manual/controllers/GetAllManuals.java
@@ -1,6 +1,7 @@
 package com.example.backend.manual.controllers;
 
 import com.example.backend.manual.model.Manual;
+import com.example.backend.manual.pojo.ManualJSON;
 import com.example.backend.manual.repository.ManualRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -19,11 +21,15 @@ public class GetAllManuals {
     ManualRepository manualRepository;
     @GetMapping("/all")
     @Transactional
-    public ResponseEntity<List<Manual>> getAllManuals(){
-        final List<Manual> manuals = (List<Manual>) manualRepository.findAll();
-        return !manuals.isEmpty()
-                ? new ResponseEntity<>(manuals, HttpStatus.OK)
-                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public List<ManualJSON> getAllManuals(){
+        List<Manual> manualList = manualRepository.findAll();
+        List<ManualJSON> allManuals = new ArrayList<>();
+        for (int i = 0; i < manualList.size(); i++) {
+            Manual currentManual = manualList.get(i);
+            ManualJSON newManualJson = new ManualJSON(currentManual.getId(), currentManual.getManual(), currentManual.getDescription());
+            allManuals.add(newManualJson);
+        }
+        return allManuals;
     }
 
 }
